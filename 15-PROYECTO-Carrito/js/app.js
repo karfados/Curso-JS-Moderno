aaf12bdd9a6d14e5a50125c62ca88bad325e30f5
Karfados <sm.benavides1@gmail.com>
2023-08-08 09:17:48 -0600

    inicio de proyecto de carrito

---
+++ 15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,7 @@
+//Variables 
+
+const carrito=document.querySelector("#carrito");
+const cursos=document.querySelector("#lista-cursos");
+const contenedorCarrito=document.querySelector("#lista-carrito tbody");
+const vaciarCarro=document.querySelector("#vaciar-carrito");
+
